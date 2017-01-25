f8bac5de6907a52f815e474f76f940e4e8283dc4
Francesco Giordano <francesco.giordano@buildo.io>
2017-01-25 17:15:35 +0100

    delete unused isReady file

--- src/isReady.js
+++
@@ -1,7 +0,0 @@
-import every from 'lodash/every';
-
-export default ({ readyState, ...props }) => {
-  return every(readyState, (rs, k) => (
-    props[k] !== void 0 && rs.error === void 0
-  ));
-};
