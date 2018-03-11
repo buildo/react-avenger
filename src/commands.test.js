10087525db6d82cb6fafa48a084142858d24e957
Giovanni Gonzaga <giovanni@buildo.io>
2018-03-11 17:06:20 +0100

    add semi-useless commands test

---
+++ src/commands.test.js
@@ -0,0 +1,26 @@
+/* global jest, describe, it, expect */
+import * as React from 'react';
+import declareCommands from './commands';
+import { Command } from 'avenger';
+import * as t from 'io-ts';
+import { mount } from 'enzyme';
+
+const makeCommands = () => {
+  const doFoo = Command({
+    params: { token: t.string },
+    run: () => Promise.resolve()
+  });
+  return { doFoo };
+};
+
+describe('declareCommands', () => {
+
+  it('works', async () => {
+    const { doFoo } = makeCommands();
+    const render = jest.fn(JSON.stringify.bind(JSON));
+    const WithDoFoo = declareCommands({ doFoo })(render);
+    const mounted = mount(<WithDoFoo token='foo' />);
+    expect(mounted).toMatchSnapshot();
+  });
+
+});
