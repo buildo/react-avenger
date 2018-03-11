cf22be395dff336415bc3e625c9f3b80709f0c6f
Giovanni Gonzaga <giovanni@buildo.io>
2018-03-11 17:00:10 +0100

    add commands basic type tests and rewrite commands.js

--- src/commands.js
+++ src/commands.js
@@ -1,35 +1,16 @@
 import React from 'react';
-import pick from 'lodash/pick';
 import mapValues from 'lodash/mapValues';
-import PropTypes from 'prop-types';
 import displayName from './displayName';
+import { runCommand } from 'avenger';
 
-export const CommandsContextTypes = {
-  commands: PropTypes.object
-};
-
-export default function declareCommands(ids) {
+export default function declareCommands(commands) {
   return Component => {
     return class CommandsWrapper extends React.Component {
-      static contextTypes = CommandsContextTypes;
-
       static displayName = displayName('commands')(Component);
 
-      constructor(props, context) {
-        super(props, context);
-
-        if (process.env.NODE_ENV !== 'production') {
-          ids.forEach(id => {
-            if (!context.commands[id]) {
-              console.warn('react-avenger:commands', `command '${id}' not found! commands declaration is: ${ids}`); // eslint-disable-line no-console
-            }
-          });
-        }
-      }
-
       componentWillMount() {
-        this._commands = mapValues(pick(this.context.commands, ids), cmd => params => {
-          return cmd({ ...this.props, ...params });
+        this._commands = mapValues(commands, cmd => params => {
+          return runCommand(cmd, { ...this.props, ...params });
         });
       }
 
--- src/index.d.ts
+++ src/index.d.ts
@@ -15,4 +15,20 @@ type DeclareQueriesReturn<Decl extends Queries> = (
   <InnerProps extends QueriesInnerProps<Decl>>(Component: React.ComponentType<InnerProps>) => React.ComponentType<QueriesOuterProps<InnerProps, Decl>>
 ) & { Props: QueriesInnerProps<Decl> };
 
-export function declareQueries<Decl extends Queries>(declaration: Decl): DeclareQueriesReturn<Decl>;
+export function declareQueries<Decl extends Queries>(declaration: Decl): DeclareQueriesReturn<Decl>
+
+type CommandsProps<Decl extends Commands> = { [k in keyof Decl]: Partial<Decl[k]['_A']> }[keyof Decl];
+
+type CommandsInnerProps<Decl extends Commands> = {
+  // TODO: currently commands also automagically forward all props to every command invocation
+  // thus `params: Decl[k]['_A']` is too strict
+  [k in keyof Decl]: (params: Decl[k]['_A']) => Promise<Decl[k]['_P']>
+};
+
+type CommandsOuterProps<InnerProps extends {}, Decl extends Commands> = ObjectOmit<InnerProps, keyof Decl> & CommandsProps<Decl>;
+
+type DeclareCommandsReturn<Decl extends Commands> = (
+  <InnerProps extends CommandsInnerProps<Decl>>(Component: React.ComponentType<InnerProps>) => React.ComponentType<CommandsOuterProps<InnerProps, Decl>>
+) & { Props: CommandsInnerProps<Decl> };
+
+export function declareCommands<Decl extends Commands>(declaration: Decl): DeclareCommandsReturn<Decl>
