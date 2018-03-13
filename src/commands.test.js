5b7436de3e4c8a3b117fcfd510af3275f8be944e
Giovanni Gonzaga <giovanni@buildo.io>
2018-03-13 09:47:44 +0100

    add one more test for commands

--- src/commands.test.js
+++ src/commands.test.js
@@ -5,20 +5,32 @@ import { Command } from 'avenger';
 import * as t from 'io-ts';
 import { mount } from 'enzyme';
 
-const makeCommands = () => {
-  const doFoo = Command({
-    params: { token: t.string },
-    run: () => Promise.resolve()
-  });
-  return { doFoo };
-};
-
 describe('declareCommands', () => {
   it('works', async () => {
-    const { doFoo } = makeCommands();
+    const doFoo = Command({ params: { token: t.string }, run: () => Promise.resolve() });
     const render = jest.fn(JSON.stringify.bind(JSON));
     const WithDoFoo = declareCommands({ doFoo })(render);
     const mounted = mount(<WithDoFoo token="foo" />);
     expect(mounted).toMatchSnapshot();
+  });
+
+  it('runs a command forwarding all props', async () => {
+    const run = jest.fn(Promise.resolve.bind(Promise));
+    const doFoo = Command({
+      params: { token: t.string },
+      run
+    });
+    class C extends React.Component {
+      componentDidMount() {
+        this.props.doFoo();
+      }
+      render() {
+        return null;
+      }
+    }
+    const WithDoFoo = declareCommands({ doFoo })(C);
+    mount(<WithDoFoo token="foo" foo="bar" />);
+    expect(run.mock.calls.length).toBe(1);
+    expect(run.mock.calls[0][0]).toEqual({ token: 'foo', foo: 'bar' });
   });
 });
