0c8c30d539b9914829b72a535e0492aeecf6b1d3
Giovanni Gonzaga <giovanni@buildo.io>
2018-03-20 16:24:05 +0100

    try to reproduce

--- src/queries.test.js
+++ src/queries.test.js
@@ -1,7 +1,7 @@
 /* global jest, describe, it, expect */
 import * as React from 'react';
 import declareQueries from './queries';
-import { Query } from 'avenger';
+import { Query, invalidate } from 'avenger';
 import * as t from 'io-ts';
 import { mount } from 'enzyme';
 
@@ -47,5 +47,20 @@ describe('declareQueries', () => {
     expect(render.mock.calls[2][0].foo.ready).toBe(true);
     expect(render.mock.calls[2][0].foo.loading).toBe(false);
     expect(render.mock.calls[2][0].foo.value).toBe('foo');
+  });
+
+  it('invalidating a near-sync query, the default debouncing avoids a rerender with ready=false', async () => {
+    const { foo } = makeQueries();
+    const render = jest.fn(JSON.stringify.bind(JSON));
+    const WithFoo = declareQueries({ foo })(render);
+    mount(<WithFoo token="foo" />);
+    await sleep(10);
+    expect(render.mock.calls.length).toBe(2);
+    invalidate({ foo }, { token: 'foo' });
+    await sleep(10);
+    const newCalls = render.mock.calls.slice(1);
+    expect(newCalls.length).toBe(2);
+    const alwaysReady = newCalls.every(call => call[0].foo.ready);
+    expect(alwaysReady).toBe(true);
   });
 });
