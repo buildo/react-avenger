24b1e4812f0454ce55868d8fba7baeba5eae9472
Eric Camellini <eric.camellini@buildo.io>
2018-04-09 12:56:17 +0200

    Updating commands tests

--- src/commands.test.js
+++ src/commands.test.js
@@ -1,9 +1,10 @@
 /* global jest, describe, it, expect */
 import * as React from 'react';
 import declareCommands from './commands';
-import { Command } from 'avenger';
+import { Command, Query } from 'avenger';
 import * as t from 'io-ts';
 import { mount } from 'enzyme';
+import sleep from './sleep';
 
 describe('declareCommands', () => {
   it('works', async () => {
@@ -15,22 +16,33 @@ describe('declareCommands', () => {
   });
 
   it('runs a command forwarding all props', async () => {
-    const run = jest.fn(Promise.resolve.bind(Promise));
+    const fetch = jest.fn(({ token }) => (Promise.resolve(token)));
+    const bar = Query({
+      params: { token: t.string },
+      fetch
+    });
+
+    const run = jest.fn(() => (Promise.resolve()));
     const doFoo = Command({
-      params: { token: t.string, foo: t.string },
+      dependencies: { bar },
+      params: { foo: t.string },
       run
     });
     class C extends React.Component {
       componentDidMount() {
         this.props.doFoo();
+        expect(Object.keys(this.props)).toEqual(['token','foo','doFoo'])
       }
       render() {
         return null;
       }
     }
     const WithDoFoo = declareCommands({ doFoo })(C);
-    mount(<WithDoFoo token="foo" foo="bar" />);
+    mount(<WithDoFoo token="token" foo="foo" />);
+    expect(fetch.mock.calls.length).toBe(1);
+    expect(fetch.mock.calls[0][0]).toEqual({ token: 'token' });
+    await sleep(1000);
     expect(run.mock.calls.length).toBe(1);
-    expect(run.mock.calls[0][0]).toEqual({ token: 'foo', foo: 'bar' });
+    expect(run.mock.calls[0][0]).toEqual({ foo: "foo", bar: 'token', token: 'token' });
   });
 });
