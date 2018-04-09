631e75e8f5812ea7c2184c61dcec5379f6e144f4
Eric Camellini <eric.camellini@buildo.io>
2018-04-09 14:42:17 +0200

    Making new tests and reverted al tests back as they were before

--- src/commands.test.js
+++ src/commands.test.js
@@ -16,33 +16,69 @@ describe('declareCommands', () => {
   });
 
   it('runs a command forwarding all props', async () => {
-    const fetch = jest.fn(({ token }) => Promise.resolve(token));
-    const bar = Query({
-      params: { token: t.string },
-      fetch
-    });
-
-    const run = jest.fn(() => Promise.resolve());
+    const run = jest.fn(Promise.resolve.bind(Promise));
     const doFoo = Command({
-      dependencies: { bar },
-      params: { foo: t.string },
+      params: { token: t.string, foo: t.string },
       run
     });
     class C extends React.Component {
       componentDidMount() {
         this.props.doFoo();
-        expect(Object.keys(this.props)).toEqual(['token', 'foo', 'doFoo']);
       }
       render() {
         return null;
       }
     }
     const WithDoFoo = declareCommands({ doFoo })(C);
-    mount(<WithDoFoo token="token" foo="foo" />);
-    expect(fetch.mock.calls.length).toBe(1);
-    expect(fetch.mock.calls[0][0]).toEqual({ token: 'token' });
+    mount(<WithDoFoo token="foo" foo="bar" />);
+    expect(run.mock.calls.length).toBe(1);
+    expect(run.mock.calls[0][0]).toEqual({ token: 'foo', foo: 'bar' });
+  });
+
+  it('runs a command with invalidations and dependencies forwarding all props', async () => {
+    const fetchFoo = jest.fn(({ fooParam }) => Promise.resolve(fooParam));
+    const foo = Query({
+      params: { fooParam: t.string },
+      fetch: fetchFoo
+    });
+    const bar = Query({
+      params: { barParam: t.string },
+      fetch: ({ barParam }) => Promise.resolve(barParam)
+    });
+
+    const run = jest.fn(({ token, foo }) => {
+      expect(token).toBe('token');
+      expect(foo).toBe('foo');
+      return Promise.resolve();
+    });
+    const doFoo = Command({
+      invalidates: { bar },
+      dependencies: { foo },
+      params: { token: t.string },
+      run
+    });
+    //eslint-disable-next-line
+    class C extends React.Component {
+      componentDidMount() {
+        this.props.doFoo();
+        expect(Object.keys(this.props)).toEqual(['token', 'fooParam', 'barParam', 'doFoo']);
+      }
+      render() {
+        return null;
+      }
+    }
+    const WithDoFoo = declareCommands({ doFoo })(C);
+    mount(<WithDoFoo token="token" fooParam="foo" barParam="bar" />);
+    expect(fetchFoo.mock.calls.length).toBe(1);
+    expect(fetchFoo.mock.calls[0][0]).toEqual({ fooParam: 'foo' });
     await sleep(1000);
     expect(run.mock.calls.length).toBe(1);
-    expect(run.mock.calls[0][0]).toEqual({ foo: 'foo', bar: 'token', token: 'token' });
+    expect(run.mock.calls[0][0]).toEqual({
+      foo: 'foo',
+      token: 'token',
+      // dependencies and invalidations params also passed to run as params, currently
+      fooParam: 'foo',
+      barParam: 'bar'
+    });
   });
 });
--- typings-checker/commands.tsx
+++ typings-checker/commands.tsx
@@ -1,37 +1,13 @@
 import { declareCommands } from '../src';
-import {
-  Command,
-  CommandReturn,
-  CommandArgsNoInvsNoDeps,
-  QueryReturn,
-  CommandArgs,
-  CommandRun
-} from 'avenger';
+import { CommandReturn } from 'avenger';
 import * as React from 'react';
-import * as t from 'io-ts';
 
-declare const foo: QueryReturn<{ foo: string }, string>;
-declare const bar: QueryReturn<{ bar: string }, string>;
-
-const commandArgs = {
-  invalidates: { foo },
-  dependencies: { bar },
-  params: { token: t.string },
-  run: (({ token, bar }) => {}) as (params: { token: string; bar: string }) => Promise<void>
-};
-const doFoo = Command(commandArgs);
-
+declare const doFoo: CommandReturn<{ token: string }, void>;
 const withDoFoo = declareCommands({ doFoo });
 
 // test the result type of declareCommands().Props
 type WithDoFooProps = {
-  doFoo: (
-    params: {
-      token: string;
-      bar: string;
-      foo: string;
-    }
-  ) => Promise<void>;
+  doFoo: (params: { token: string }) => Promise<void>;
 };
 declare var withDoFooProps: WithDoFooProps;
 withDoFooProps = withDoFoo.Props;
@@ -39,13 +15,7 @@ withDoFooProps = withDoFoo.Props;
 declare const C1: React.ComponentType<typeof withDoFoo.Props>;
 const WithDoFoo = withDoFoo(C1);
 
-<WithDoFoo foo={'foo'} token={'token'} bar={'bar'} />;
+<WithDoFoo />;
 
 // $ExpectError Type 'number' is not assignable to type 'string | undefined'.
 <WithDoFoo token={10} />;
-
-// $ExpectError Type 'number' is not assignable to type 'string | undefined'.
-<WithDoFoo foo={10} />;
-
-// $ExpectError Type 'number' is not assignable to type 'string | undefined'.
-<WithDoFoo bar={10} />;
