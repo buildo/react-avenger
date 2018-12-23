a62422619137c8c660619e43a69739871a1917cc
Federico Sordillo <federicosordillo@gmail.com>
2018-12-23 19:24:52 +0100

    fix type test

--- typings-checker/queries.tsx
+++ typings-checker/queries.tsx
@@ -30,10 +30,10 @@ declare type C1Props = { bar: number } & typeof withFoo.Props;
 declare const C1: React.ComponentType<C1Props>;
 const WithFoo = withFoo(C1);
 
-// $ExpectError Property 'bar' is missing in type '{}'.
+// $ExpectError Property 'bar' is missing in type '{}' but required in type 'Pick<C1Props, "bar">'.
 <WithFoo />;
 
-// $ExpectError Property 'token' is missing in type '{ bar: number; }'.
+// $ExpectError Property 'token' is missing in type '{ bar: number; }' but required in type '{ token: string; }'.
 <WithFoo bar={10} />;
 
 <WithFoo bar={10} token="foo" />;
@@ -72,7 +72,7 @@ withFooAndBarProps = withFooAndBar.Props;
 declare const C2: React.ComponentType<typeof withFooAndBar.Props>;
 const WithFooAndBar = withFooAndBar(C2);
 
-// $ExpectError Property 'token' is missing in type '{}'.
+// $ExpectError Property 'token' is missing in type '{}' but required in type '{ token: string; }'.
 <WithFooAndBar />;
 
 <WithFooAndBar token="bar" />;
@@ -85,8 +85,8 @@ const withFooAndBaz = declareQueries({ foo, baz });
 declare const C3: React.ComponentType<typeof withFooAndBaz.Props>;
 const WithFooAndBaz = withFooAndBaz(C3);
 
-// $ExpectError Property 'token2' is missing in type '{ token: string; }'.
+// $ExpectError  Property 'token2' is missing in type '{ token: string; }' but required in type '{ token2: string; }'.
 <WithFooAndBaz token="token" />;
-// $ExpectError Property 'token' is missing in type '{ token2: string; }'.
+// $ExpectError Property 'token' is missing in type '{ token2: string; }' but required in type '{ token: string; }'.
 <WithFooAndBaz token2="token2" />;
 <WithFooAndBaz token="token" token2="token2" />;
