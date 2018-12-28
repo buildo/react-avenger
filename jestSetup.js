13627ad11124915f291c9f6fe8c5d9f3392c1b46
Giovanni Gonzaga <giovanni@buildo.io>
2018-12-28 18:09:38 +0100

    remove unused deps

--- jestSetup.js
+++ jestSetup.js
@@ -1,5 +1,5 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
